ae15dfd6430d829323c17ff3cdc32149b2d56e17
William Velazquez <william.velazquez.a@gmail.com>
2020-07-25 13:15:00 -0500

    (Components): Creating Layout Component, Global Styles and useToggle Custom Hook  master

---
+++ src/components/Layout.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import GlobalStyles from '../styled/GlobalStyles';
+import Header from './Header';
+
+import LOGO from '../assets/images/logo.svg';
+
+const Content = styled.div`
+  padding: 1em;
+  padding-top: 100px;
+`;
+
+const Layout = (props) => {
+  const { children } = props;
+  return (
+    <>
+      <GlobalStyles />
+      <Header logo={LOGO} logoAlt='Simetrik Logo' />
+      <Content>
+        {
+          children
+        }
+      </Content>
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/styled/GlobalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    background: #ffffff;
+    font-family: 'Poppins', sans-serif, system-ui;
+  }
+  button {
+    font-family: 'Poppins', sans-serif, system-ui;
+  }
+`;
+
+export default GlobalStyles;
